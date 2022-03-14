9f99bdc13e0d0cac6cdc8fb911d067c19296b9a7
narendar naganolla <narendarreddy85@gmail.com>
2022-03-14 12:18:33 -0400

    modify project

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import javax.swing.*;
+import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainApp {
-static Logger logger=Logger.getLogger("MianApp");
+static Logger logger=Logger.getLogger("MainApp");
     public static void main(String... args){
 
-        logger.info("message sent");
+        logger.info("message sent"+ new Date());
     }
 
 }
--- src/test/java/org/example/CalTest.java
+++ src/test/java/org/example/CalTest.java
@@ -1,16 +1,16 @@
-package org.example;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-
-public class CalTest {
-
-    @Test
-    public void sum() {
-
-        Cal c= new Cal();
-        int sum = c.sum(10);
-        Assert.assertEquals(20,sum);
-    }
-}
+//package org.example;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//
+//public class CalTest {
+//
+//    @Test
+//    public void sum() {
+//
+//        Cal c= new Cal();
+//        int sum = c.sum(10);
+//        Assert.assertEquals(20,sum);
+//    }
+//}
